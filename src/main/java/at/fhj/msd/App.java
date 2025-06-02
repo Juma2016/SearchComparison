cd1cd663227a09beb33d6b37c8d8678ffe66b043
Mario Shenouda <mario.shenouda@edu.fh-joanneum.at>
2025-06-02 14:11:35 +0200

    bearbeiten an der App.java damit wird die 100000 funktioniert

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -2,9 +2,8 @@ package at.fhj.msd;
 
 public class App {
   
- public static void main(String[] args) {
+   public static void main(String[] args) {
         int[] sizes = {100, 1000, 10000, 100000};
-        int runs = 2;
         StatsCollector stats = new StatsCollector();
 
         for (int size : sizes) {
@@ -12,33 +11,46 @@ public class App {
             int[] sorted = ArrayGenerator.sortedCopy(unsorted);
             System.out.println("\nTesting array of size: " + size);
 
-            for (int i = 0; i < runs; i++) {
-                long start = System.nanoTime();
-                for (int x = 1; x <= size; x++) SearchAlgorithms.linearSearch(unsorted, x);
-                long end = System.nanoTime();
-                stats.record("LinearSearch(" + size + ")", end - start);
+            long start = System.nanoTime();
+            for (int j = 0; j < 1000; j++) {
+            int x = (int)(Math.random() * size) + 1;
+            SearchAlgorithms.linearSearch(unsorted, x);
+}
 
-                start = System.nanoTime();
-                for (int x = 1; x <= size; x++) SearchAlgorithms.binarySearch(sorted, x);
-                end = System.nanoTime();
-                stats.record("BinarySearch(" + size + ")", end - start);
+            long end = System.nanoTime();
+            stats.record("LinearSearch(" + size + ")", end - start);
 
-                start = System.nanoTime();
-                for (int x = 1; x <= size; x++) SearchAlgorithms.interpolationSearch(sorted, x);
-                end = System.nanoTime();
-                stats.record("InterpolationSearch(" + size + ")", end - start);
+            start = System.nanoTime();
+            for (int j = 0; j < 1000; j++) {
+            int x = (int)(Math.random() * size) + 1;
+             SearchAlgorithms.linearSearch(unsorted, x);
+}
 
-                start = System.nanoTime();
-                for (int x = 1; x <= size; x++) SearchAlgorithms.quadraticBinarySearch(sorted, x);
-                end = System.nanoTime();
-                stats.record("QuadraticBinarySearch(" + size + ")", end - start);
-            }
+            end = System.nanoTime();
+            stats.record("BinarySearch(" + size + ")", end - start);
+
+            start = System.nanoTime();
+            for (int j = 0; j < 1000; j++) {
+            int x = (int)(Math.random() * size) + 1;
+            SearchAlgorithms.linearSearch(unsorted, x);
+}
+
+            end = System.nanoTime();
+            stats.record("InterpolationSearch(" + size + ")", end - start);
+
+            start = System.nanoTime();
+            for (int j = 0; j < 1000; j++) {
+            int x = (int)(Math.random() * size) + 1;
+            SearchAlgorithms.linearSearch(unsorted, x);
+}
+
+            end = System.nanoTime();
+            stats.record("QuadraticBinarySearch(" + size + ")", end - start);
 
             stats.printStatistics("LinearSearch(" + size + ")");
             stats.printStatistics("BinarySearch(" + size + ")");
             stats.printStatistics("InterpolationSearch(" + size + ")");
             stats.printStatistics("QuadraticBinarySearch(" + size + ")");
-        
         }
     }
 }
