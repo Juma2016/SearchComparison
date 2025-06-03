70adf77b59ebfd602f93fe3ddc76e085951b070b
Rozaliya Aleksandrova <rozaliya.aleksandrova@edu.fh-joanneum.at>
2025-06-03 12:03:25 +0200

    add robust input validation

--- src/main/java/at/fhj/msd/SearchAlgorithms.java
+++ src/main/java/at/fhj/msd/SearchAlgorithms.java
@@ -23,7 +23,7 @@ public class SearchAlgorithms {
     int low = 0, high = a.length - 1;
     
     while (low <= high && x >= a[low] && x <= a[high]) {
-
+        
         // Special case: All elements in the range are equal
         if (a[low] == a[high]) {
             return (a[low] == x) ? low : -1;
@@ -40,7 +40,7 @@ public class SearchAlgorithms {
         // Check boundaries
         if (pos < low || pos > high) {
             pos = (low + high) / 2;  
-
+        }
         if (a[pos] == x) {
             return pos;
         } else if (a[pos] < x) {
@@ -50,18 +50,32 @@ public class SearchAlgorithms {
         }
     }
     return -1;
-    }
+}
+    
+    public static int quadraticBinarySearch(int[] a, int x) {
+    int left = 0, right = a.length - 1;
 
-    // public static int quadraticBinarySearch(int[] a, int x) {
-    //     int left = 0, right = a.length - 1;
-    //     while (left <= right) {
-    //         int mid = left + ((right - left) * (right - left))/ ((a.length * a.length) / 4);
-    //         if (mid >= a.length) mid = (left + right) / 2;
-    //         if (a[mid] == x) return mid;
-    //         if (a[mid] < x) left = mid + 1;
-    //         else right = mid - 1;
-    //     }
-    //     return -1;
-    // }
+    // To prevent endless loops
+    int maxTries = 1000; 
+
+    while (left <= right && maxTries-- > 0) {
+        int range = right - left;
+        int denominator = (a.length * a.length) / 4;
+        // Prevent division by zero
+        if (denominator == 0) denominator = 1; 
+
+        int mid = left + (range * range) / denominator;
+
+        // If mid is out of bounds, we use normal arithmetic.
+        if (mid < left || mid > right) {
+            mid = left + (right - left) / 2;
+        }
+
+        if (a[mid] == x) return mid;
+        if (a[mid] < x) left = mid + 1;
+        else right = mid - 1;
     }
+    
+    return -1; 
+}
 }
